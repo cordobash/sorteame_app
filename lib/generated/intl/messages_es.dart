63246b3990a2265240f9433f5e476d93e22830ea
Isaias Cordova <igerardo0808@gmail.com>
2024-05-25 19:44:59 -0600

    El proyecto ya arranca de nuevo pero con algunos errores que solucionar

--- lib/generated/intl/messages_es.dart
+++ lib/generated/intl/messages_es.dart
@@ -21,39 +21,5 @@ class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'es';
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-        "PAGINA_ACERCADE": MessageLookupByLibrary.simpleMessage(""),
-        "PAGINA_AJUSTES": MessageLookupByLibrary.simpleMessage(""),
-        "activateAnimations":
-            MessageLookupByLibrary.simpleMessage("Activar animacion"),
-        "cancel": MessageLookupByLibrary.simpleMessage("Cancelar"),
-        "career":
-            MessageLookupByLibrary.simpleMessage("(Desarrollador de software)"),
-        "choicecolor": MessageLookupByLibrary.simpleMessage(
-            "Selecciona uno de los siguientes colores:"),
-        "contact": MessageLookupByLibrary.simpleMessage("Vias de contacto"),
-        "countdown": MessageLookupByLibrary.simpleMessage("Cuenta regresiva"),
-        "deleteAfter": MessageLookupByLibrary.simpleMessage(
-            "Eliminar participantes post sorteo"),
-        "developedby": MessageLookupByLibrary.simpleMessage("Desarrollado por"),
-        "duplicatedNames":
-            MessageLookupByLibrary.simpleMessage("Permitir nombres duplicados"),
-        "editparticipant":
-            MessageLookupByLibrary.simpleMessage("Editar participante"),
-        "general": MessageLookupByLibrary.simpleMessage("General"),
-        "language": MessageLookupByLibrary.simpleMessage("Seleccionar idioma"),
-        "letsmakereal":
-            MessageLookupByLibrary.simpleMessage("Hagamos realidad tus ideas!"),
-        "maxCells": MessageLookupByLibrary.simpleMessage(
-            "Maximo de celdas por fila en modo mosaico"),
-        "modalchoicecolor":
-            MessageLookupByLibrary.simpleMessage("Seleccionar un color"),
-        "ok": MessageLookupByLibrary.simpleMessage("Aceptar"),
-        "personalization":
-            MessageLookupByLibrary.simpleMessage("Personalizacion"),
-        "showDeleteDialog": MessageLookupByLibrary.simpleMessage(
-            "Mostrar dialogo al eliminar participante"),
-        "themeApp": MessageLookupByLibrary.simpleMessage("Color principal"),
-        "thememode": MessageLookupByLibrary.simpleMessage("Activar modo oscuro")
-      };
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{};
 }
